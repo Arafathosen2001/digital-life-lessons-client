9b355570cf60368f2aedfb22da6cc56bdd4762e7
Arafathosen2001 <aarafat985@gmail.com>
2026-06-20 18:19:10 +0600

    Add A Page For Admin Manag Lessons || 2026-06-20 06:19 PM

---
+++ src/app/dashboard/admin/manage-lessons/page.jsx
@@ -0,0 +1,10 @@
+export default function ManageLessons() {
+    return (
+      <div>
+        <h1 className="text-xl font-bold">Manage Lessons</h1>
+        <p className="opacity-70">
+          Admin can delete / feature / review lessons.
+        </p>
+      </div>
+    );
+  }
