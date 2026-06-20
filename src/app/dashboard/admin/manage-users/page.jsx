4a663e75ccc9d7000293de8af7c5be68b0bcddef
Arafathosen2001 <aarafat985@gmail.com>
2026-06-20 18:18:10 +0600

    Add A Page For Admin Manage User || 2026-06-20 06:18 PM

---
+++ src/app/dashboard/admin/manage-users/page.jsx
@@ -0,0 +1,10 @@
+export default function ManageUsers() {
+    return (
+      <div>
+        <h1 className="text-xl font-bold">Manage Users</h1>
+        <p className="opacity-70">
+          Table of users with role update & delete.
+        </p>
+      </div>
+    );
+  }
