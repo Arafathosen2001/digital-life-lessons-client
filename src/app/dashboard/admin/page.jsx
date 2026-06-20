c2ba7a68afe13fcd481ce23baddd5b0edd9d5376
Arafathosen2001 <aarafat985@gmail.com>
2026-06-20 18:16:46 +0600

    Add A Page For Admin || 2026-06-20 06:16 PM

---
+++ src/app/dashboard/admin/page.jsx
@@ -0,0 +1,12 @@
+export default function AdminHome() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">
+          Admin Dashboard Overview
+        </h1>
+        <p className="opacity-70 mt-2">
+          Total users, lessons, reports analytics will show here.
+        </p>
+      </div>
+    );
+  }
